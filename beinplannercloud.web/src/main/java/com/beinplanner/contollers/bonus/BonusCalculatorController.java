22ed8765c240781bd112cfca2a3a9de306899c32
user <user@192.168.1.107>
2017-12-15 20:18:35 +0200

    RestController

--- beinplannercloud.web/src/main/java/com/beinplanner/contollers/bonus/BonusCalculatorController.java
+++ beinplannercloud.web/src/main/java/com/beinplanner/contollers/bonus/BonusCalculatorController.java
@@ -1,5 +1,8 @@
 package com.beinplanner.contollers.bonus;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value="/ctrl/bonus/")
 public class BonusCalculatorController {
 
 }
