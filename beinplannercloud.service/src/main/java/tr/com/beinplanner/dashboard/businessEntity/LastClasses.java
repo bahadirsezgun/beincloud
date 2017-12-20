3183b337366b651b8fbbfb5910f3e4424d47a3fb
user <user@192.168.1.106>
2017-12-20 19:07:34 +0200

    Dashboard process continue

---
+++ beinplannercloud.service/src/main/java/tr/com/beinplanner/dashboard/businessEntity/LastClasses.java
@@ -0,0 +1,42 @@
+package tr.com.beinplanner.dashboard.businessEntity;
+
+import java.util.List;
+
+import tr.com.beinplanner.schedule.dao.ScheduleMembershipPlan;
+import tr.com.beinplanner.schedule.dao.ScheduleTimePlan;
+
+public class LastClasses {
+
+	private List<ScheduleTimePlan> stpTW;
+	private List<ScheduleTimePlan> stpNW;
+	
+	private List<ScheduleMembershipPlan> stpMTW;
+	private List<ScheduleMembershipPlan> stpMNW;
+	
+	public List<ScheduleTimePlan> getStpTW() {
+		return stpTW;
+	}
+	public void setStpTW(List<ScheduleTimePlan> stpTW) {
+		this.stpTW = stpTW;
+	}
+	public List<ScheduleTimePlan> getStpNW() {
+		return stpNW;
+	}
+	public void setStpNW(List<ScheduleTimePlan> stpNW) {
+		this.stpNW = stpNW;
+	}
+	public List<ScheduleMembershipPlan> getStpMTW() {
+		return stpMTW;
+	}
+	public void setStpMTW(List<ScheduleMembershipPlan> stpMTW) {
+		this.stpMTW = stpMTW;
+	}
+	public List<ScheduleMembershipPlan> getStpMNW() {
+		return stpMNW;
+	}
+	public void setStpMNW(List<ScheduleMembershipPlan> stpMNW) {
+		this.stpMNW = stpMNW;
+	}
+	
+	
+}
